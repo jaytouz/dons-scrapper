9b89a47e567f9e31813a87604ec5553e83ba5aca
jaytouz <tousijer@gmail.com>
2022-02-20 15:17:52 -0500

    log file update with date

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import logging
-
+from datetime import datetime
 # local package
 from src.runner.process import DonationScrapperRunner
 from src.scrapper.enums import PoliticalPartiesValues, LeadershipCandidateValues
 
 
 def main():
-    logging.basicConfig(filename='../output/multiprocess/multiThread.log',
+    outputDir = "../output/multiprocess/"
+    now = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    logging.basicConfig(filename=outputDir + f"donation_scrapper_{now}.log",
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         encoding='utf-8', level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
     runner = DonationScrapperRunner(
         "../output/multiprocess/", years=["2022", "2021", "2020", "2019"], parties=[PoliticalPartiesValues.PCQ_CPQ, PoliticalPartiesValues.PLQ_QLP])
-    runner.run(poolSize=4)
+    runner.run()
 
 
 if __name__ == "__main__":
