60003007dff7bf705ed4ccc4bee00d358773a79c
jaytouz <tousijer@gmail.com>
2022-02-20 23:39:10 -0500

    auto create path for log

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import logging
 from datetime import datetime
+
 # local package
 from src.runner.process import DonationScrapperRunner
 from src.scrapper.enums import PoliticalPartiesValues, LeadershipCandidateValues
 
 
 def main():
-    outputDir = "../output/multiprocess/"
+    outputDir = "../output/fulldata/"
     now = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    if(not os.path.exists(outputDir)):
+        os.makedirs(outputDir)
     logging.basicConfig(filename=outputDir + f"donation_scrapper_{now}.log",
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         encoding='utf-8', level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
     runner = DonationScrapperRunner(
-        "../output/multiprocess/", years=["2022", "2021", "2020", "2019"], parties=[PoliticalPartiesValues.PCQ_CPQ, PoliticalPartiesValues.PLQ_QLP])
+        "../output/multiprocess/", years=[], parties=[])
     runner.run()
 
 
